333bea87582f6adc863ff504f6c4f89e70c5c9e5
Rafael Ferreira <rafael.ferreira@before.com.br>
2019-06-24 22:35:11 -0400

    wip

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -13,13 +13,13 @@ Séries
 <a href="{{ route('formSerie') }}" class="btn btn-dark mb-3">Adicionar</a>
     <ul class="list-group">
         @foreach($series as $serie)
-            <li class="list-group-item">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
                 {{ $serie->nome }} 
                 <form method="post" action="/series/{{ $serie->id }}"
                     onsubmit="return confirm('Deseja remover {{ addslashes($serie->nome) }} ?')">
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-danger">EXCLUIR</button>
+                    <button class="btn btn-danger "><i class="fas fa-trash"></i></button>
                 </form>
             </li>
 
